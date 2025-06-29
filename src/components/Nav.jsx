a78804d0f59be6e629816c0087f2d56b8ce70759
Prashant Patel <56prashant09@gmail.com>
2025-06-29 17:49:31 +0530

    hoverEffect

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+const navLinkClass = ({ isActive }) =>
+  `px-3 py-1 rounded ${isActive ? "text-blue-600 font-bold" : "text-black"}`;
+
 const Nav = () => {
   return (
-    <div className="flex flex-col  justify-center h-[10vh] bg-white   gap-4 my-2 p-2 rounded shadow-sm">
+    <div className="flex flex-col  justify-center h-[10vh] bg-gray-100   gap-4 my-2 p-2 rounded shadow-sm">
       <div className="flex justify-between items-center w-full">
         <h1 className="text-2xl text-blue-600 font-bold ">
           {" "}
           <img className="w-40 scale-110" src="/public/Logo.jpg" alt="" />
         </h1>
+
         <div className="flex gap-10 font-medium">
-          <NavLink to="/dashboard">Dashboard</NavLink>
-          <NavLink to="/add">Add Receipts</NavLink>
-          <NavLink to="/receipt">Receipt</NavLink>
+          <NavLink to="/dashboard" className={navLinkClass}>
+            Dashboard
+          </NavLink>
+          <NavLink to="/add" className={navLinkClass}>
+            Add Receipts
+          </NavLink>
+          <NavLink to="/receipt" className={navLinkClass}>
+            Receipt
+          </NavLink>
         </div>
       </div>
-     
     </div>
   );
 };
