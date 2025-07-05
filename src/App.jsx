c1c7e8362bb4aea697f5bd2cd2116978f41a04ff
Rahul Kumar Sharma <rahulkumarsharma7898@gmail.com>
2025-07-05 13:10:49 +0530

    not rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import React from "react";
-import AddReceipts from "./pages/AddReceipts";
-import Nav from "./components/Nav";
-import DashBoard from "./pages/Dashboard";
-import Receipt from "./pages/Receipt";
-import { Route, Routes } from "react-router-dom";
 
+import Nav from "./components/Nav";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Routing from "./utils/Routing";
+
+
 const App = () => {
   return (
     <BrowserRouter basename="/TrackBill/">
--- src/Utils/Routing.jsx
+++ src/Utils/Routing.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import { Route, Routes, Navigate } from "react-router-dom";
 
+import { Route, Routes, Navigate } from "react-router-dom";
 import AddReceipts from '../pages/AddReceipts';
 import DashBoard from '../pages/Dashboard';
 import Receipt from '../pages/Receipt';
