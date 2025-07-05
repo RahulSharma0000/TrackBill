5359c0bf46df727415bcb2eb9af428ea7da820d9
Rahul Kumar Sharma <rahulkumarsharma7898@gmail.com>
2025-07-05 13:01:34 +0530

    some change in routing

--- src/Utils/Routing.jsx
+++ src/Utils/Routing.jsx
@@ -1,25 +1,19 @@
-import React from 'react'
-import AddReceipts from '../pages/AddReceipts'
-import Nav from '../components/Nav'
-import DashBoard from '../pages/Dashboard'
-import Receipt from '../pages/Receipt'
-import{Route ,Routes,Navigate} from "react-router-dom"
+import React from 'react';
+import { Route, Routes, Navigate } from "react-router-dom";
 
-
+import AddReceipts from '../pages/AddReceipts';
+import DashBoard from '../pages/Dashboard';
+import Receipt from '../pages/Receipt';
 
 const Routing = () => {
   return (
-    
-      <Routes >
-        <Route path="/" element={<Navigate to="/dashboard" replace />}></Route>
-        <Route path="/dashboard" element={<DashBoard/>}></Route>
-        <Route path="/add" element={<AddReceipts/>}></Route>
-        <Route path="/receipt" element={<Receipt/>}></Route>
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Navigate to="/dashboard" replace />} />
+      <Route path="/dashboard" element={<DashBoard />} />
+      <Route path="/add" element={<AddReceipts />} />
+      <Route path="/receipt" element={<Receipt />} />
+    </Routes>
+  );
+};
 
-  )
-}
-
-export default Routing
-
-
+export default Routing;
